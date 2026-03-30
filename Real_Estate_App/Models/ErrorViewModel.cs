7be9b6b6f4961fa891ffe6bcf39457a60deb35e2
Ravi162910 <ravip357090@gmail.com>
2026-03-30 14:38:19 +1300

    Add project files.

---
+++ Real_Estate_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Real_Estate_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
