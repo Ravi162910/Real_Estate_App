c7621a9002cdbbaefdc16368a70c3c538dd5b12e
Ravi162910 <ravip357090@gmail.com>
2026-04-04 16:05:37 +1300

    First Stage of Agent model done, still planning

---
+++ Real_Estate_App/Controllers/AgentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Real_Estate_App.Data;
+
+namespace Real_Estate_App.Controllers
+{
+    public class AgentController : Controller
+    {
+        private readonly AgentDbContext _context;
+        public AgentController(AgentDbContext agentDbContext) 
+        {
+            _context = agentDbContext;
+        }
+
+        public IActionResult AgentOptions() 
+        {
+            var Agentlist = _context.Agents_Set.ToList();
+            return View(Agentlist);
+        }
+    }
+}
---
+++ Real_Estate_App/Models/Agent_SeedData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Real_Estate_App.Data;
+
+namespace Real_Estate_App.Models
+{
+    public class Agent_SeedData
+    {
+        public static void Initialize(IServiceProvider serviceP) 
+        {
+            using (var agentcontext = new AgentDbContext(serviceP.GetRequiredService<DbContextOptions<AgentDbContext>>())) 
+            {
+                if (agentcontext.Agents_Set.Any()) 
+                {
+                    return;
+                }
+                agentcontext.Agents_Set.AddRange(
+                    new Agent 
+                    {
+                        Agent_FullName = "Greg Turner",
+                        Agent_AgencyName = "Clark & Co Realty"
+                    },
+                    new Agent 
+                    {
+                        Agent_FullName = "Kelly Graham",
+                        Agent_AgencyName = "Harcourts"
+                    },
+                    new Agent 
+                    {
+                        Agent_FullName = "Peter Remmington",
+                        Agent_AgencyName = "Ray White"
+                    });
+                agentcontext.SaveChanges();
+            }
+        }
+    }
+}
