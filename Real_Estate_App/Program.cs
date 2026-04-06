ec3b3c8df5c76b370255eacf50d3eb3bc3c520ef
Ravi162910 <ravip357090@gmail.com>
2026-04-06 18:54:52 +1200

    Made a few changes to get the app working

--- Real_Estate_App/Program.cs
+++ Real_Estate_App/Program.cs
@@ -16,14 +16,12 @@ if (provider == "SqlServer")
 {
     builder.Services.AddDbContext<UserAdminDbContext>(options => options.UseSqlServer(connectionStringSql));
     builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionStringSql));
-    builder.Services.AddDbContext<AgentDbContext>(options => options.UseSqlServer(connectionStringSql));
     //builder.Services.AddDbContext<>(options => options.UseSqlServer(connectionStringSql));
 }
 else
 {
     builder.Services.AddDbContext<UserAdminDbContext>(options => options.UseSqlite(connectionString));
     builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
-    builder.Services.AddDbContext<AgentDbContext>(options => options.UseSqlite(connectionString));
 }
 
 builder.Services.AddScoped<IEmailService, EmailService>();
@@ -34,8 +32,6 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope()) //Important for Seeding Database data & Auto Migrations when the app runs
 {
     var services = scope.ServiceProvider;
-
-    Agent_SeedData.Initialize(services);
 }
 
 
